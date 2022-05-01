a03847d99610aa5b90fc429798a61bdea6e7590f
Nathanael <dev@nathabonfim59.com>
2022-05-01 06:32:48 -0300

    feat(tabela): adiciona estilos e itens com base no state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import TabelaHead from './componentes/TabelaHead';
 import TabelaBody from './componentes/TabelaBody';
 import TabelaFoot from './componentes/TabelaFoot';
+import "./index.css";
 
-function App() {
-  return (
-    <table className="tabela">
-      <TabelaHead/>
-      <TabelaBody/>
-      <TabelaFoot/>
-    </table>
-  );
+class App extends Component {
+  state = {
+    livros: [
+      {
+        id: "98-85-7522-632-2",
+        titulo: "CSS Grid Layout",
+        autor: "Maurício Samy Silva",
+      },
+      {
+        id: "97808507522-677-3",
+        titulo: "Node Essencial",
+        autor: "Ricardo R. Lecheta",
+      },
+      {
+        id: "978-85-7522-512-7",
+        titulo: "Aprendendo Material Design",
+        autor: "Kyle Mew",
+      },
+    ]
+  };
+
+  render() {
+    return (
+      <table className="tabela">
+        <TabelaHead/>
+        <TabelaBody livros={this.state.livros}/>
+        <TabelaFoot/>
+      </table>
+    );
+  }
 }
 
 export default App;
